2ba0fb057dc56ff6c8a9d87a2f18ad4f993a349e
Hancoson <hancoson@163.com>
2016-05-04 17:07:43 +0800

    this fun

---
+++ src/scripts/lib/utils.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Guoxing.han on 2016-5-04.
+ */
+const utils={
+    alert:(...args)=>{
+        alert(args);
+    }
+};
+
+export {utils};
--- src/scripts/template-string.js
+++ src/scripts/template-string.js
@@ -2,14 +2,35 @@
  * Created by Guoxing.han on 2016-5-03.
  */
 import $ from 'jquery';
+import {utils} from './lib/utils';
 const Template = {
-    init: () => {
-        let context = $('#test'),
-            x=1,
-            y=2;
+    x: 0,
+    init: (o) => {
+        console.log(this);
+        Template.context = $('#test');
+        Template.Param();
+        utils.alert(123, 1, 3);
+        Template.test();
 
-        context.html(`Ceci n'est pas une chaine ${x}.`);
-
+    },
+    Param: (x = 'a', y = 'b') => {
+        Template.context.html(`Lions and ${x} and ${y}! Oh my!`);
+    },
+    test: () => {
+        //console.log(this.x);
+        $('#event').on('click',function(){
+            console.log(this,11334);
+        });
     }
 };
-$(Template.init);
+Template.init();
+
+const obj = {
+    birth: 1990,
+    getAge: function () {
+        let b = this.birth; // 1990
+        let fn = () => new Date().getFullYear() - this.birth; // this指向obj对象
+        return fn();
+    }
+};
+obj.getAge(); // 26
